f88fc1f0272835caced99cbf2e70fc7517e47c0f
Richard Skott <riprask@live.com>
2021-03-28 19:01:06 +0100

    created initial auth container and skaffold setup

---
+++ auth/src/index.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import {json} from 'body-parser';
+
+const app = express();
+app.use(json());
+
+app.get('/api/users/currentUser', (req, res) => {
+    res.send('Hi there!')
+})
+
+app.listen(3000, () => {
+    console.log(`Listerning on port ${3000}!!!!!`)
+})
