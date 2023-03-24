1a87da1baa0b251f368a4657b95a7bc148b51d36
Ripraskel <3710141+Ripraskel@users.noreply.github.com>
2023-03-24 16:17:12 +0000

    Section 10 progress so far

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,5 @@
-import express, { Request, Response, NextFunction } from 'express';
-import {json} from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-
-import { errorHandler } from './middleware/error-handler';
-import { NotFoundError } from './errors/not-found-error';
-
-const app = express();
-
-app.set('trust proxy', true);
-
-app.use(json());
-app.use(cookieSession({
-    signed: false,
-    secure: true
-}))
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.all('*', async (req: Request, res: Response, next: NextFunction) => {
-    next(new NotFoundError());
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
     if (!process.env.JWT_KEY) {
@@ -43,10 +12,10 @@ const start = async () => {
         console.error(err)
     }
     
+    app.listen(3000, () => {
+        console.log(`Listerning on port ${3000}!!!!!`)
+    })
 };
 
-app.listen(3000, () => {
-    console.log(`Listerning on port ${3000}!!!!!`)
-})
 
 start();
